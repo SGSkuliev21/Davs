4b91717de6a34d02055d4cca5c59b89114d1e0a6
VDVichev21 <VDVichev21@codingburgas.bg>
2024-01-07 17:38:43 +0200

    Add character customization

--- Davs/Davs/Davs.cpp
+++ Davs/Davs/Davs.cpp
@@ -1,22 +1,65 @@
 #include "raylib.h"
 
-int main()
-{
+int main() {
     const int screenWidth = 800;
     const int screenHeight = 450;
 
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+    InitWindow(screenWidth, screenHeight, "Davs");
 
-    SetTargetFPS(60);
+    Image image1 = LoadImage("Davs/img/brown.png");
+    Texture2D texture1 = LoadTextureFromImage(image1);
+    UnloadImage(image1);
 
-    while (!WindowShouldClose()) 
-    {
+    Image image2 = LoadImage("Davs/img/ginger.png");
+    Texture2D texture2 = LoadTextureFromImage(image2);
+    UnloadImage(image2);
+
+    Vector2 imagePosition = { (float)(screenWidth - texture1.width) / 2, (float)(screenHeight - texture1.height) / 2 };
+
+    bool showForwardArrow = false;
+    bool showBackwardArrow = false;
+
+    while (!WindowShouldClose()) {
+        if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+            Rectangle forwardArrowBounds = { 10, 10, 30, 30 };
+            Rectangle backwardArrowBounds = { screenWidth - 40, 10, 30, 30 };
+            Vector2 mousePoint = GetMousePosition();
+
+            if (CheckCollisionPointRec(mousePoint, forwardArrowBounds)) {
+                UnloadTexture(texture1);
+                texture1 = texture2;
+            }
+            else if (CheckCollisionPointRec(mousePoint, backwardArrowBounds)) {
+                UnloadTexture(texture1);
+                texture1 = LoadTextureFromImage(image1);
+            }
+
+            showForwardArrow = true;
+            showBackwardArrow = true;
+        }
+        else {
+            showForwardArrow = false;
+            showBackwardArrow = false;
+        }
+
         BeginDrawing();
-
         ClearBackground(RAYWHITE);
 
-        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        if (showForwardArrow) {
+            DrawText(">", 15, 15, 20, BLACK);
+            DrawRectangle(10, 10, 30, 30, WHITE);
+        }
+
+        if (showBackwardArrow) {
+            DrawText("<", screenWidth - 35, 15, 20, BLACK);
+            DrawRectangle(screenWidth - 40, 10, 30, 30, WHITE);
+        }
+
+        DrawTexture(texture1, static_cast<int>(imagePosition.x), static_cast<int>(imagePosition.y), WHITE);
 
         EndDrawing();
     }
+    UnloadTexture(texture1);
+    UnloadTexture(texture2);
+
 }
